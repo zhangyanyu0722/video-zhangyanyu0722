d4f3f2ae21734faf7421d14c3080117ae414a6b6
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-02-20 17:11:00 -0500

    Add files via upload

---
+++ task2/queue_test.py
@@ -0,0 +1,53 @@
+import pytest
+from tweepy_get import tweepy_get
+from image2video import image_to_video
+import multiprocessing
+import time
+import threading
+import queue
+import os
+
+def worker():
+  i = 0
+  while True:
+    item = q.get()
+    if item is None:
+      print("Break ! Because item is None")
+      break
+    tweepy_get(item)
+    image_to_video(item)
+    i += 1
+    print("---------------Thread Done--------------")
+    q.task_done()
+
+def queue_1(keyNames, number_thread):
+  q = queue.Queue()
+  threads = []
+
+  for i in range(number_thread):
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
+
+  for item in keyNames:
+    q.put(item)
+
+  q.join()
+
+  for i in range(number_thread):
+    q.put(None)
+
+  for t in threads:
+    t.join()
+
+def queue_test():
+  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+  number_thread = 4
+  queue_1(keyNames, number_thread)
+  assert os.path.exists('BU_Tweets.avi')
+
+
+
+
+
+
