76195d94e2623a42e28ed2a4971017ca1fb777d5
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-02-20 16:57:19 -0500

    Update ThreadsVSProcesses_test.py

--- ThreadsVSProcesses/ThreadsVSProcesses_test.py
+++ ThreadsVSProcesses/ThreadsVSProcesses_test.py
@@ -6,8 +6,34 @@
 
 from ThreadsVSProcesses import *
 import pytest
+import multiprocessing
+import threading
+import time
 
 def test_ThreadsVSProcesses():
-  pass
-
-
+  cpu_n_iters = 8
+  sleep = 1
+  cpu_count = multiprocessing.cpu_count()
+  input_params = [
+    (CpuThread, cpu_n_iters),
+    (CpuProcess, cpu_n_iters),
+    (IoThread, sleep),
+    (IoProcess, sleep),
+  ]
+  header = ['nthreads']
+  for thread_class, _ in input_params:
+    header.append(thread_class.__name__)
+  print(' '.join(header))
+  for nthreads in range(1, 2 * cpu_count):
+    results = [nthreads]
+    for thread_class, work_size in input_params:
+      start_time = time.time()
+      threads = []
+      for i in range(nthreads):
+        thread = thread_class(work_size)
+        threads.append(thread)
+        thread.start()
+      for i, thread in enumerate(threads):
+        thread.join()
+        results.append(time.time() - start_time)
+    print(' '.join('{:.6e}'.format(result) for result in results))
