1618577ce35b546cd6cbd62fbe82ac4251c56e52
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-02-21 09:17:41 -0500

    Update queue_test.py

--- task2/queue_test.py
+++ task2/queue_test.py
@@ -8,14 +8,23 @@ import threading
 import queue
 import os
 
-def test_queue():
-  pass
+def test_twitter_get():
+  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+  for key in keynames:
+    tweepy_get(key)
+  assert os.path.exists('img/BU_Tweets10.png')
+  assert os.path.exists('img/BostonDynamics11.png')
+  assert os.path.exists('img/realDonaldTrump12.png')
+  assert os.path.exists('img/WHO14.png')
+  assert os.path.exists('img/TIME15.png')
+
+    
 # *****************************************************************************************************
 # If you want to test the following part, please input the twitter API KEY in the key.txt
 # *****************************************************************************************************
 
-  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
-  number_thread = 4
+#   keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+#   number_thread = 4
 #   queue_1(keyNames, number_thread)
 #   assert os.path.exists('BU_Tweets.avi')
 #   assert os.path.exists('BU_ece.avi')
