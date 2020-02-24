3383a9a857af8f91d3b2268d3323c643a0713c71
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-02-24 17:46:49 -0500

    Update queue_test.py

--- task2/queue_test.py
+++ task2/queue_test.py
@@ -11,38 +11,43 @@ import os
 # If you want to test the following part, please input the twitter API KEY in the key.txt
 # *****************************************************************************************************
 def test_twitter_get():
-  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
-  for key in keyNames:
-    pass # delate this line
-#     tweepy_get(key)
-#   assert os.path.exists('img/BU_Tweets1.png') == True
-#   assert os.path.exists('img/BostonDynamics11.png') == True
-#   assert os.path.exists('img/realDonaldTrump12.png') == True
+  assert os.path.exists('img/BU_Tweets1.png') == True
+  assert os.path.exists('img/BostonDynamics11.png') == True
+  assert os.path.exists('img/realDonaldTrump12.png') == True
+  print("Twitter get is passed")
   
 def test_image2video():
-  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
-  for key in keyNames:
-    pass # delate this line
-#     image_to_video(key)
-#   assert os.path.exists('BU_ece.avi') == True
-#   assert os.path.exists('BostonDynamics.avi') == True
-#   assert os.path.exists('realDonaldTrump.avi') == True
-#   assert os.path.exists('WHO.avi') == True
-    
+  assert os.path.exists('BU_ece.avi') == True
+  assert os.path.exists('BostonDynamics.avi') == True
+  assert os.path.exists('realDonaldTrump.avi') == True
+  assert os.path.exists('WHO.avi') == True
+  print("Image to video is passed")
+
 def test_queue():
-  keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
-  number_thread = 4
-#   queue_1(keyNames, number_thread)
-#   assert os.path.exists('BU_Tweets.avi') == True
-#   assert os.path.exists('BU_ece.avi') == True
-#   assert os.path.exists('BostonDynamics.avi') == True
-#   assert os.path.exists('realDonaldTrump.avi') == True
-#   assert os.path.exists('WHO.avi') == True
-#   assert os.path.exists('TIME.avi') == True
-#   assert os.path.exists('celtics.avi') == True
-#   assert os.path.exists('nytimes.avi') == True
-#   assert os.path.exists('washingtonpost.avi') == True
-#   assert os.path.exists('BillGates.avi') == True
+  assert os.path.exists('BU_Tweets.avi') == True
+  assert os.path.exists('BU_ece.avi') == True
+  assert os.path.exists('BostonDynamics.avi') == True
+  assert os.path.exists('realDonaldTrump.avi') == True
+  assert os.path.exists('WHO.avi') == True
+  assert os.path.exists('TIME.avi') == True
+  assert os.path.exists('celtics.avi') == True
+  assert os.path.exists('nytimes.avi') == True
+  assert os.path.exists('washingtonpost.avi') == True
+  assert os.path.exists('BillGates.avi') == True
+  print("Queue is passed")
+
+
+keyNames = ['BU_Tweets', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME', 'celtics', 'nytimes', 'washingtonpost', 'BillGates']
+number_thread = 4
+# queue_1(keyNames, number_thread)
+# test_twitter_get()
+# test_queue()
+# test_image2video()
+
+
+
+
+
 
 
 
